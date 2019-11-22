888b3064537155cb675abe777e1f1460870258e2
Lundez <hampus.londogard@gmail.com>
2019-11-22 17:24:40 +0100

    Test to fail

--- src/test/kotlin/com/londogard/SmileTests.kt
+++ src/test/kotlin/com/londogard/SmileTests.kt
@@ -8,7 +8,7 @@ import org.junit.Test
 class MyLibraryTest {
     @Test fun testTokenization() {
         assertEquals(listOf("hey", "there", "you", "are", "londogard"), "hey there you are londogard".words(filter=StopWordFilter.NONE))
-        assertEquals(listOf("hey", "londogard"), "hey there you are londogard".words())
+        assertEquals(listOf("hey", "londogardd"), "hey there you are londogard".words())
         // TODO add more test coverage
     }
 }
